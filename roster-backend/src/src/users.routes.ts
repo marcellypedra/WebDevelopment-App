921e070bfebfb02fcac72b66882b3bc39f04e1b2
MARCELLY PEDRA <20040674@mydbs.ie>
2025-02-24 12:42:03 +0000

    update

--- roster-backend/src/src/users.routes.ts
+++
@@ -1,7 +0,0 @@
-import * as express from "express";
-import { ObjectId } from "mongodb";
-import { collections } from "./database";
-
-export const usersRouter = express.Router();
-usersRouter.use(express.json());
-
