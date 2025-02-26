71fe3e37bcaa376fb712b364d5fc0a9bcc26de80
marcellypedra <20040674@mydbs.ie>
2025-02-26 10:53:30 +0000

    atrributes standardization

--- roster-backend/src/users.ts
+++ roster-backend/src/users.ts
@@ -3,7 +3,7 @@ import * as mongodb from "mongodb";
 export interface Users {
     _id?: mongodb.ObjectId;
     name: string;
-    phoneNumber: number;
+    phoneNumber: string;
     email: string;
     address: string;
     DOB: Date;
